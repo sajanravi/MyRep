2b44181d59a525b139a4a372b7394a3bc0bf413e
sajanravi <sajan.ravi123@gmail.com>
2017-06-06 15:59:27 +0530

    commited Sample GitPj

---
+++ Sample GitPj/src/com/sample_gitpj/main/Main.java
@@ -0,0 +1,12 @@
+package com.sample_gitpj.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is a sample");
+		
+	}
+
+}
